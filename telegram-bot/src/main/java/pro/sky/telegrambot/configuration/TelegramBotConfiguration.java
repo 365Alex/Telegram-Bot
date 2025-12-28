b83278d515bbcbe96c028f2c7efcb91ec9b0de53
master <aleshenko_ira_2017@mail.ru>
2025-12-28 10:06:46 +0300

    telegram bot

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -23,14 +23,5 @@ public class TelegramBotConfiguration {
 
     }
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:telegrambotdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("password");
-        return dataSource;
-    }
 
 }
