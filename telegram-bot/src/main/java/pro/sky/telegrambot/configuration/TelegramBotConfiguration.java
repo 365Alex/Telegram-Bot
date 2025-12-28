dae1028835e678d36799c7c7fc0a321f3ccb3e20
master <aleshenko_ira_2017@mail.ru>
2025-12-28 10:22:50 +0300

    telegram bot

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -5,9 +5,7 @@ import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class TelegramBotConfiguration {
